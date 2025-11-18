05d27bddd3521782ab971f832c3d5d8a86d51779
Mattia Domizio s341987 <s341987@studenti.polito.it>
2025-11-18 18:58:41 +0100

    Story-8-Completed+Documented

--- server/src/services/mapperService.ts
+++ server/src/services/mapperService.ts
@@ -54,6 +54,10 @@ export function mapReportDAOToDTO(dao: ReportDAO): Report {
     document: {
       description: dao.document?.Description,
       photos: dao.document?.Photos
-    }
+    },
+    //? aggiunti per la getAssigned (non la retrieve che prende solo i pending, ma in generale la getAssigned che prende tutti i report assegnati anche non-pending)
+    state: dao.state as any,
+    reason: dao.reason || undefined,
+    assignedOfficerId: (dao as any).assignedOfficerId || undefined
   };
 }
