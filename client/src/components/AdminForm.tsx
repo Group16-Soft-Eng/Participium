eadb5c26cee24aaf5dabb74a5ee0e2996272bd1e
Pietro Pantani <128312132+P11etro@users.noreply.github.com>
2025-11-21 14:09:38 +0100

    fixed issue regarding officer roles in admin UI

--- client/src/components/AdminForm.tsx
+++ client/src/components/AdminForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Grid, MenuItem, Select, Stack, TextField } from "@mui/material";
+import { Box, Button, Container, FormControl, Grid, InputLabel, MenuItem, Select, Stack, TextField } from "@mui/material";
 import './Forms.css';
 import { Form, useNavigate } from "react-router-dom";
 import { useActionState, useEffect, useState } from "react";
@@ -72,40 +72,46 @@ export function AdminForm({ setShowForm }: AdminFormProps) {
             <form action={formAction}>
                 <Grid container spacing={2} maxWidth="sm">
                     <Grid size={6}>
-                        <TextField id="name" name="name" label="Name" variant="outlined" fullWidth required/>
+                        <TextField id="name" name="name" label="Name" variant="outlined" fullWidth required />
                     </Grid>
                     <Grid size={6}>
-                        <TextField id="surname" name="surname" label="Surname" variant="outlined" fullWidth required/>
+                        <TextField id="surname" name="surname" label="Surname" variant="outlined" fullWidth required />
                     </Grid>
                     <Grid size={12}>
-                        <TextField id="username" name="username" label="Username" variant="outlined" fullWidth required/>
+                        <TextField id="username" name="username" label="Username" variant="outlined" fullWidth required />
                     </Grid>
                     <Grid size={12}>
-                        <TextField id="email" name="email" label="Email" variant="outlined" fullWidth required/>
+                        <TextField id="email" name="email" label="Email" variant="outlined" fullWidth required />
                     </Grid>
                     <Grid size={12}>
-                        <TextField id="cemail" name="cemail" label="Confirm Email" variant="outlined" fullWidth required/>
+                        <TextField id="cemail" name="cemail" label="Confirm Email" variant="outlined" fullWidth required />
                     </Grid>
                     <Grid size={6}>
-                        <TextField id="password" name="password" label="Password" variant="outlined" type="password" fullWidth required/>
+                        <TextField id="password" name="password" label="Password" variant="outlined" type="password" fullWidth required />
                     </Grid>
                     <Grid size={6}>
-                        <TextField id="confirm-password" name="confirm-password" label="Confirm Password" variant="outlined" type="password" fullWidth required/>
+                        <TextField id="confirm-password" name="confirm-password" label="Confirm Password" variant="outlined" type="password" fullWidth required />
                     </Grid>
-                    <Grid size={12}>
-                        <Select id="office" name="office" label="Office" variant="outlined" fullWidth defaultValue={''} required> {
-                            officeTypes.map((type) => (<MenuItem key={type} value={type}>{type}</MenuItem>
-                            ))
-                        }
-                        </Select>
-                    </Grid>
-                    <Grid size={12}>
-                        <Select id="role" name="role" label="Role" variant="outlined" fullWidth defaultValue={''} required> {
-                            officerTypes.map((type) => (<MenuItem key={type} value={type}>{type.replaceAll('_', ' ')}</MenuItem>
-                            ))
-                        }
-                        </Select>
-                    </Grid>
+                    <FormControl fullWidth>
+                        <InputLabel id="role-select-label">Officer Role</InputLabel>
+                        <Grid size={12}>
+                            <Select id="role" name="role" label="Officer Role" variant="outlined" fullWidth defaultValue={''} required> {
+                                officerTypes.map((type) => (<MenuItem key={type} value={type}>{type.replaceAll('_', ' ')}</MenuItem>
+                                ))
+                            }
+                            </Select>
+                        </Grid>
+                    </FormControl>
+                    <FormControl fullWidth>
+                        <InputLabel id="office-select-label">Office</InputLabel>
+                        <Grid size={12}>
+                            <Select id="office" name="office" label="Office" variant="outlined" fullWidth defaultValue={''} required> {
+                                officeTypes.map((type) => (<MenuItem key={type} value={type}>{type}</MenuItem>
+                                ))
+                            }
+                            </Select>
+                        </Grid>
+                    </FormControl>
                     {error && <Grid size={12}><p className="error">{error}</p></Grid>}
                     <Grid size={6}>
                         <Button variant="outlined" onClick={() => setShowForm(false)} fullWidth>Go Back</Button>
