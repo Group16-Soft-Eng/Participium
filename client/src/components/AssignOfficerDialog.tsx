cf3e782d0bd8fdc774c3848b9c82a0c282ef3c46
Pietro Pantani <pietro.pantani.03@gmail.com>
2026-01-03 17:16:51 +0100

    removed code smells from client

--- client/src/components/AssignOfficerDialog.tsx
+++ client/src/components/AssignOfficerDialog.tsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Box, IconButton, Select, Stack, MenuItem, FormControl, InputLabel, Grid } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Box, Select, Stack, MenuItem, FormControl, InputLabel, Grid } from '@mui/material';
 import type { OfficerReport } from '../services/reportService';
 import { getOfficersByOffice, assignOfficer } from "../API/API";
-import { useEffect } from 'react';
 
 interface Props {
     open: boolean;
     report: OfficerReport | null;
     office: string;
     onClose: () => void;
-    successfulAssign: (reportId: number) => void; 
+    successfulAssign: (reportId: number) => void;
     failedAssign: (reportId: number) => void;
 }
 
@@ -49,42 +48,40 @@ const AssignOfficerDialog: React.FC<Props> = ({ open, report, onClose, office, s
         }
     }
     return (
-        <>
-            <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
-                <form onSubmit={handleSubmit}>
-                    <DialogTitle>Choose the officer to assign the report to {report?.title}</DialogTitle>
-                    <DialogContent>
-                        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3, mt: 2 }}>
-                            <Stack spacing={2}>
-                                <Box>
+        <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
+            <form onSubmit={handleSubmit}>
+                <DialogTitle>Choose the officer to assign the report to {report?.title}</DialogTitle>
+                <DialogContent>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3, mt: 2 }}>
+                        <Stack spacing={2}>
+                            <Box>
+                                {officers.length > 0 &&
+                                    `Available Officers for ${office} office:`}
+                            </Box>
+                            <FormControl fullWidth>
+                                <Grid size={12}>
+                                    {officers.length === 0 && <Box>No officers available for this office.</Box>}
                                     {officers.length > 0 &&
-                                        `Available Officers for ${office} office:`}
-                                </Box>
-                                <FormControl fullWidth>
-                                    <Grid size={12}>
-                                        {officers.length === 0 && <Box>No officers available for this office.</Box>}
-                                        {officers.length > 0 &&
-                                            <>
-                                                <InputLabel id="officer-select-label">Select an officer</InputLabel>
-                                                <Select id="officer" name="officer" label="Select an officer" variant="outlined" fullWidth defaultValue={''} required> {
-                                                    officers.map((officer) => (<MenuItem key={officer.id} value={officer.id}>{officer.name} {officer.surname}</MenuItem>
-                                                    ))
-                                                }
-                                                </Select>
-                                            </>
-                                        }
-                                    </Grid>
-                                </FormControl>
-                            </Stack>
-                        </Box>
-                    </DialogContent>
-                    <DialogActions>
-                        {officers.length > 0 && <Button variant="contained" type="submit">Assign</Button>}
-                        <Button variant="outlined" onClick={onClose}>Close</Button>
-                    </DialogActions>
-                </form>
-            </Dialog>
-        </>
+                                        <>
+                                            <InputLabel id="officer-select-label">Select an officer</InputLabel>
+                                            <Select id="officer" name="officer" label="Select an officer" variant="outlined" fullWidth defaultValue={''} required> {
+                                                officers.map((officer) => (<MenuItem key={officer.id} value={officer.id}>{officer.name} {officer.surname}</MenuItem>
+                                                ))
+                                            }
+                                            </Select>
+                                        </>
+                                    }
+                                </Grid>
+                            </FormControl>
+                        </Stack>
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    {officers.length > 0 && <Button variant="contained" type="submit">Assign</Button>}
+                    <Button variant="outlined" onClick={onClose}>Close</Button>
+                </DialogActions>
+            </form>
+        </Dialog>
     );
 };
 
