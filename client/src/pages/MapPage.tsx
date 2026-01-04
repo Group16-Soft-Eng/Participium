338cb5dab20f8945fbeda53b587480de91e89816
Pietro Pantani <pietro.pantani.03@gmail.com>
2026-01-04 19:07:26 +0100

    unlogged users can now view reports

--- client/src/pages/MapPage.tsx
+++ client/src/pages/MapPage.tsx
@@ -186,7 +186,6 @@ const MapPage: React.FC = () => {
         p: 2,
         bgcolor: '#f8f9fa'
       }} elevation={2}>
-        {logged ?
           <>
             <Typography variant="h6" gutterBottom>
               {searchCoords ? `Reports near location (${filteredReports.length})` : `Reports on map (${reports.length})`}
@@ -246,7 +245,7 @@ const MapPage: React.FC = () => {
                           {authorName}
                           {` • ${new Date(r.createdAt).toLocaleDateString()}`}
                         </Typography>
-                        {(r.author?.username != username && (
+                        {(logged && r.author?.username != username && (
                           <>
                             {!followedReports.some(report => report.id == r.id) && <Button variant='contained' sx={{ marginLeft: 2 }} onClick={() => follow(r.id)}>Follow</Button>}
                             {followedReports.some(report => report.id == r.id) && <Button variant='outlined' sx={{ marginLeft: 2 }} onClick={() => unfollow(r.id)}>Unfollow</Button>}
@@ -261,8 +260,6 @@ const MapPage: React.FC = () => {
               })}
             </List>
           </>
-          : <Typography variant="h6" gutterBottom>Please log in to view the available reports.</Typography>
-        }
       </Paper>
     </Box>
   );
