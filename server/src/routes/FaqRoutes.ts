5ac3ff41a1224888cd46adc5c48439e5fd13976c
Luca Amoroso s336860 <lucaamoroso.0117@gmail.com>
2026-01-05 12:56:31 +0100

    Added error management and test for FAQ Route

--- server/src/routes/FaqRoutes.ts
+++ server/src/routes/FaqRoutes.ts
@@ -6,35 +6,51 @@ const router = Router({mergeParams : true});
 
 
 router.get("/" , async (req, res) => {
-    const faqRepo = new FaqRepository();
-    const faqs = await faqRepo.getAllFaqs();
-    
-    res.status(200).json(faqs);
+    try {
+        const faqRepo = new FaqRepository();
+        const faqs = await faqRepo.getAllFaqs();
+        
+        res.status(200).json(faqs);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 router.post("/" , authenticateToken, requireUserType([OfficerRole.MUNICIPAL_ADMINISTRATOR, OfficerRole.MUNICIPAL_PUBLIC_RELATIONS_OFFICER, OfficerRole.TECHNICAL_OFFICE_STAFF]), async (req, res) => {
-    const faqRepo = new FaqRepository();
-    const { question, answer } = req.body;
-    
-    const newFaq = await faqRepo.createFaq(question, answer);
-    res.status(201).json(newFaq);
+    try {
+        const faqRepo = new FaqRepository();
+        const { question, answer } = req.body;
+        
+        const newFaq = await faqRepo.createFaq(question, answer);
+        res.status(201).json(newFaq);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 router.patch("/:faqId" , authenticateToken, requireUserType([OfficerRole.MUNICIPAL_ADMINISTRATOR, OfficerRole.MUNICIPAL_PUBLIC_RELATIONS_OFFICER, OfficerRole.TECHNICAL_OFFICE_STAFF]), async (req, res) => {
-    const faqRepo = new FaqRepository();
-    const faqId = Number.parseInt(req.params.faqId);
-    const { question, answer } = req.body;
-    
-    const updatedFaq = await faqRepo.updateFaq(faqId, question, answer);
-    res.status(200).json(updatedFaq);
+    try {
+        const faqRepo = new FaqRepository();
+        const faqId = Number.parseInt(req.params.faqId);
+        const { question, answer } = req.body;
+        
+        const updatedFaq = await faqRepo.updateFaq(faqId, question, answer);
+        res.status(200).json(updatedFaq);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 router.delete("/:faqId" , authenticateToken, requireUserType([OfficerRole.MUNICIPAL_ADMINISTRATOR, OfficerRole.MUNICIPAL_PUBLIC_RELATIONS_OFFICER, OfficerRole.TECHNICAL_OFFICE_STAFF]), async (req, res) => {
-    const faqRepo = new FaqRepository();
-    const faqId = Number.parseInt(req.params.faqId);
-    
-    await faqRepo.deleteFaq(faqId);
-    res.status(200).json({ message: "FAQ deleted successfully." });
+    try {
+        const faqRepo = new FaqRepository();
+        const faqId = Number.parseInt(req.params.faqId);
+        
+        await faqRepo.deleteFaq(faqId);
+        res.status(200).json({ message: "FAQ deleted successfully." });
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 export {router as faqRouter};
